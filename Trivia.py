8f7a6ba32db15029f81dd7684b6610386d525bb4
portasfiga1099@gmail.com <M4uc7Lz9hJIm>
2022-01-11 11:35:16 +0100

    create basic graphical interface

--- Trivia.py
+++ Trivia.py
@@ -1,4 +1,6 @@
-from PyQt5.QtWidgets import QMainWindow, QInputDialog
+from PyQt5.QtCore import QRect, QMetaObject, QCoreApplication
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QMainWindow, QInputDialog, QWidget, QGroupBox, QLabel, QFrame, QPushButton
 from Utilities import chunks
 import random
 
@@ -36,19 +38,16 @@ class MainWindow(QMainWindow):
         self.setup_questions()
 
         # start graphical interfaces
+        self.TITLE_FONT = {
+            "name": "Franklin Gothic Medium",
+            "size": 22
+        }
+        self.MINOR_TEXT_FONT = {
+            "name": "Franklin Gothic Book",
+            "size": 14
+        }
         self.init_ui()
         self.show()
-
-        self.game_loop()
-
-    def init_ui(self) -> None:
-        """
-        Create the UI of the main window
-        :return: nothing
-        """
-
-        self.setObjectName("MainWindow")
-        self.setFixedSize(620, 558)
 
     def popup_add_players(self) -> None:
         """
@@ -107,34 +106,86 @@ class MainWindow(QMainWindow):
         # adapted from https://www.kite.com/python/answers/how-to-create-a-dictionary-from-two-lists-in-python
         self.questions = dict(zip(questions_list, answers_list))
 
-    def game_loop(self) -> None:
+    def init_ui(self) -> None:
         """
-        Do the game loop (interact with graphics)
-        every step is commented
-        :return: Nothing
+        Create the UI of the main window
+        :return: nothing
         """
 
-        running = True
+        def get_title_font() -> QFont:
+            title_font = QFont()
+            title_font.setFamily(self.TITLE_FONT["name"])
+            title_font.setPointSize(self.TITLE_FONT["size"])
+            return title_font
 
-        while running:
-            self.turn += 1
+        def get_minor_font() -> QFont:
+            minor_font = QFont()
+            minor_font.setFamily(self.MINOR_TEXT_FONT["name"])
+            minor_font.setPointSize(self.MINOR_TEXT_FONT["size"])
+            return minor_font
 
-            # do a turn for every player
-            for player in self.players:
-                # 1. extract casual question
-                question_key = random.choice(list(self.questions))
-                print(question_key)
+        self.setObjectName("MainWindow")
+        self.setWindowTitle("Gravity Falls Trivia Master")
+        self.setFixedSize(800, 540)
 
-                # TODO 2. make visible the answer
+        central_widget = QWidget(self)
+        central_widget.setObjectName("central_widget")
 
-                # TODO 3. input if the player answered the right
-                if input("indovianto: ") == "si":
-                    self.players[player] += 1
+        # vertical line
+        vertical_line = QFrame(central_widget)
+        vertical_line.setGeometry(QRect(130, -20, 41, 611))
+        vertical_line.setFrameShape(QFrame.VLine)
+        vertical_line.setFrameShadow(QFrame.Sunken)
+        vertical_line.setObjectName("vertical_line")
 
-                # TODO 4. Delete the question from the possible questions
-                del self.questions[question_key]
+        # Question Box
+        question_box = QGroupBox(central_widget)
+        question_box.setGeometry(QRect(190, 30, 571, 161))
+        question_box.setFont(get_title_font())
+        question_box.setObjectName("question_box")
 
-                # TODO controlla se ha vinto
-                if self.players[player] == self.VICTORY_POINTS:
-                    running = False
-                    break
+        question_label = QLabel(question_box)
+        question_label.setGeometry(QRect(34, 45, 511, 91))
+        question_label.setFont(get_minor_font())
+        question_label.setObjectName("question_label")
+
+        # Answer Box
+        answer_box = QGroupBox(central_widget)
+        answer_box.setGeometry(QRect(190, 220, 571, 161))
+        answer_box.setFont(get_title_font())
+        answer_box.setObjectName("answer_box")
+
+        answer_label = QLabel(answer_box)
+        answer_label.setGeometry(QRect(34, 45, 511, 91))
+        answer_label.setFont(get_minor_font())
+        answer_label.setObjectName("answer_label")
+
+        # YES and NO Buttons
+        yes_button = QPushButton(central_widget)
+        yes_button.setGeometry(QRect(340, 420, 93, 81))
+        yes_button.setObjectName("yes_button")
+
+        no_button = QPushButton(central_widget)
+        no_button.setGeometry(QRect(520, 420, 93, 81))
+        no_button.setObjectName("no_button")
+
+        # translate ui
+        _translate = QCoreApplication.translate
+        question_box.setTitle(_translate("MainWindow", "DOMANDA"))
+        answer_box.setTitle(_translate("MainWindow", "RISPOSTA"))
+        no_button.setText(_translate("MainWindow", "X"))
+        yes_button.setText(_translate("MainWindow", "V"))
+
+        # set everything
+        self.setCentralWidget(central_widget)
+        QMetaObject.connectSlotsByName(self)
+
+
+# question_key = random.choice(list(self.questions))
+# del self.questions[question_key]
+# right answer? si: self.players[player] += 1
+"""
+if self.players[player] == self.VICTORY_POINTS:
+    running = False
+    break
+"""
